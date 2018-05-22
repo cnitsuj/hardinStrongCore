ae651e47ad1eb0a03c0c36ea3128558a55914d57
Justin Collazo <Justin.Collazo@outlook.com>
2018-05-21 21:04:04 -0500

    C

--- HardinStrongCore/Pages/About.cshtml.cs
+++ HardinStrongCore/Pages/About.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace HardinStrongCore.Pages
+namespace HardinStrongRazor.Pages
 {
     public class AboutModel : PageModel
     {
--- HardinStrongCore/Pages/Donate.cshtml.cs
+++ HardinStrongCore/Pages/Donate.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HardinStrongRazor.Pages
 {
-    public class IndexModel : PageModel
+    public class DonateModel : PageModel
     {
         public void OnGet()
         {
